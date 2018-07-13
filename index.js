dd8ac06a8c8afc4e90ef9476fc95cac9c74c5991
Mo Zhou Personal <mo.zhou10@yahoo.com>
2018-07-13 14:12:55 -0700

    Better comments

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/voice', (req, res) => {
   // Use the <Gather> verb to collect user input
   const gather =  response.gather({
   	input: 'speech',
-  	timeout: 5,
+  	timeout: 5, //5 seconds of waiting
     action: '/result' //check if can directly play
     });
 
@@ -55,13 +55,14 @@ app.post('/voice', (req, res) => {
 });
 
 //retrieve the song name from speech recogntion result 
+//do some error handling
 app.post('/result', (req, res) => {
 
 	const response = new VoiceResponse();
 
 	if (req.body.SpeechResult) {
 		var requestedTrack = req.body.SpeechResult.toLowerCase();
-		response.say({voice: 'woman'}, `You requested ${requestedTrack}`);
+		response.say({voice: 'woman'}, `You requested ${requestedTrack}`); //say the requested song name back
 		req.session.requestedTrack = requestedTrack; //store the speech result to a session data to refer later
 		response.redirect('/play');
 	}
@@ -78,19 +79,14 @@ app.post('/result', (req, res) => {
 
 });
 
-//let trackName = 'love'; //placeholder
 
 //playing the requested song
 app.post('/play', (req, res) => {
 	
 	const response = new VoiceResponse();
 	let trackName = req.session.requestedTrack; 
-
-	//response.say(`You requested ${trackName}`);
-	console.log('the track requested is ' + trackName);
 	
 	getTrack(trackName) 
-
 	.then(song => {
 		console.log('the song url is ' + song);
 		response.play(song);
@@ -108,16 +104,17 @@ app.post('/play', (req, res) => {
 
 //Need to figure out the NULL url issue from Spotify
 
-//utility function to get a Spotify track 
+//utility function to get a Spotify track using the helper function
+//use Promise to avoid callback hell
 //return the url of the song
 function getTrack(track) {
 	const response = new VoiceResponse();
 	return new Promise((resolve, reject) => {
 		spotify.search({ 
-			type: 'track', 
-			query: track, 
-			limit: 1,
-			market: 'US'
+			type: 'track', //asking to search for a track
+			query: track,  //track name
+			limit: 1, //return only the first matching result
+			market: 'US' //available market is the US
 		})
 		.then(function(body) {
 			try {
